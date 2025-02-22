d1629600ebaddb21c719a75686be9ae788e08e0b
Jorge Herrera <jorge.herrera.tume@gmail.com>
2025-02-22 00:13:28 -0500

    fix: login

--- src/utils/helpers/jwt-signer.helper.ts
+++ src/utils/helpers/jwt-signer.helper.ts
@@ -4,16 +4,19 @@ import { decode } from './base64.helper';
 
 export const signToken = (content: any, expiresIn: string): string => {
   try {
-    const privateKey = Buffer.from(process.env.JWT_SIGNING_PRIVATE_KEY_BASE64 as string, 'base64').toString('utf-8');
+    const privateKeyBase64 = process.env.JWT_SIGNING_PRIVATE_KEY_BASE64 as string;
+    const privateKey = Buffer.from(privateKeyBase64, 'base64').toString('utf-8');
+    console.log('privateKey', privateKey);
     return sign(
       content,
-      decode(privateKey || ''),
+      privateKey,
       {
         expiresIn: expiresIn,
         algorithm: 'RS256',
       },
     );
   } catch (error: any) {
+    console.error('JWT Signing error:', error);
     throw new InternalServerErrorException(error.message);
   }
 };
